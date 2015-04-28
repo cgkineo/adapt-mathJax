cbecaea75f2c8c2a11cb8e1f0c0ff4be84d96e66
tomgreenfield <tomgreenfield@users.noreply.github.com>
2015-04-28 09:57:16 +0100

    Committed files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # adapt-mathJax
-An extension to load MathJax into Adapt.
+
+An extension to load [MathJax](https://www.mathjax.org) into Adapt.
+
+## Installation
+
+* In this extension the default configuration for MathJax is as follows:
+```json
+"_mathJax": {
+	"_inlineConfig": {
+		"extensions": [
+			"tex2jax.js"
+		],
+		"jax": [
+			"input/TeX",
+			"output/HTML-CSS"
+		]
+	},
+	"_src": "http://cdn.mathjax.org/mathjax/latest/MathJax.js"
+}
+```
+* If this needs to be overridden, add the above to `config.json` and modify where required.
+* Copy the extension folder into the src > extensions directory and run an appropriate Grunt task.
+
+### Usage
+
+* With the default configuation, equations are processed in LaTeX format.
+* In JSON, surround LaTeX equations with `\\(``\\)` for inline mode or `\\[``\\]` for display mode.
+* Example of inline mode:
+```
+\\(x^n + y^n = z^n\\)
+```
+* Example of display mode (rendered in a separate block):
+```
+\\[f(x) = \\frac{1}{1+x}\\]
+```
+* Remember to escape any backslashes with an additional backslash `\`.
+* The Adapt loading screen is shown until MathJax has finished processing.
+* MathJax processing messages are styled to mimic the Adapt loading screen.
+* MathJax file loading messages are hidden.
---
+++ js/adapt-mathJax.js
@@ -0,0 +1,53 @@
+/*
+* MathJax
+* License - https://github.com/adaptlearning/adapt_framework/blob/master/LICENSE
+* Maintainers - Tom Greenfield
+*/
+
+define(function(require) {
+
+	var Adapt = require("coreJS/adapt");
+
+	function addScript() {
+		var config = Adapt.config.get("_mathJax");
+		var script = "<script type='text/x-mathjax-config'>";
+		var hideLoadingDiv = "MathJax.Hub.Queue(function() {\n" +
+			"	$('.loading').hide();\n" +
+			"});\n";
+		var hideLoadingMessages = "MathJax.Message.File = function(file) {\n" +
+			"	var root = MathJax.Ajax.config.root;\n" +
+			"	var rootLength = root.length;\n" +
+			"	if (file.substr(0, rootLength) === root) {\n" +
+			"		file = '[MathJax]' + file.substr(rootLength)\n" +
+			"	}\n" +
+			"	return this.Set('Loading ' + file, null, 0);\n" +
+			"},\n";
+		var inlineConfig = config && config._inlineConfig ? config._inlineConfig :
+			{ "extensions": ["tex2jax.js"], "jax": ["input/TeX", "output/HTML-CSS"] };
+		var src = config && config._src ? config._src :
+			"http://cdn.mathjax.org/mathjax/latest/MathJax.js";
+
+		script += hideLoadingDiv + hideLoadingMessages + "MathJax.Hub.Config(" +
+			JSON.stringify(inlineConfig, null, "\t") + ");</script>" +
+			"<script type='text/javascript' src='" + src + "'></script>";
+
+		$("head").append(script);
+	}
+
+	Adapt.on("app:dataReady", function() { addScript(); })
+		.on("menuView:ready pageView:ready", function(view) {
+			$(".loading").show();
+
+			if (window.MathJax) {
+				MathJax.Hub.Queue(["Typeset", MathJax.Hub, view.el], function() {
+					$(".loading").hide();
+				});
+			}
+		})
+		.on("popup:opened", function($el) {
+			_.defer(function() {
+				MathJax.Hub.Queue(["Typeset", MathJax.Hub, $el ? $el[0] : undefined]);
+			});
+		});
+
+});
