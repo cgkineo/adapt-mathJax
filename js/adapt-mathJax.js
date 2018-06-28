281ee29f803a55abf08a1679473b0200aeb7a253
Brian Quinn <brian@learningpool.com>
2018-06-28 12:19:19 +0100

    Fixes #1, resolves intermittent timing issue  (#2)
    * Fixes #1, resolves intermittent timing issue
    
    - Added new loadScript() function which adds the config, init and CDN script
    - This has a callback which waits until one script is loaded before processing the next
    - Added a polling function on onViewReady() to check that Hub actually exists before trying to call Queue()
    
    * Replaced call for checkForMathJax()
    
    * Added Adapt.wait calls (where available) - this avoids the deprecation warning on v3.

--- js/adapt-mathJax.js
+++ js/adapt-mathJax.js
@@ -1,20 +1,50 @@
 define([ "core/js/adapt" ], function(Adapt) {
 
+	function loadScript(scriptObject, callback) {
+		var head = document.getElementsByTagName('head')[0];
+		var script = document.createElement('script');
+
+		script.type = scriptObject.type || 'text/javascript';
+
+		if (scriptObject.src) {
+			script.src = scriptObject.src;
+		}
+
+		if (scriptObject.text) {
+			script.text = scriptObject.text;
+		}
+
+		if (callback) {
+			// Then bind the event to the callback function.
+			// There are several events for cross browser compatibility.
+			script.onreadystatechange = callback;
+			script.onload = callback;
+		}
+
+		// Append the <script> tag.
+		head.appendChild(script);
+	}
+
 	function setUpMathJax() {
+		Adapt.wait ? Adapt.wait.begin() : Adapt.trigger("plugin:beginWait");
+
 		var config = Adapt.config.get("_mathJax");
 		var inlineConfig = config ? config._inlineConfig : {
 				"extensions": [ "tex2jax.js" ],
 				"jax": [ "input/TeX", "output/HTML-CSS" ]
 		};
-		var src = config ?
-			config._src :
-			"//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js";
-		var $init = $("<script/>", { src: "assets/mathJaxInit.js" });
-		var $config = $("<script/>", { type: "text/x-mathjax-config" });
-		var $src = $("<script/>", { src: src });
+		var src = config ? config._src : "//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js";
 
-		$config[0].text = "MathJax.Hub.Config(" + JSON.stringify(inlineConfig) + ");";
-		$("head").append($init, $config, $src);
+		loadScript({ 
+			type: "text/x-mathjax-config",
+			text: "MathJax.Hub.Config(" + JSON.stringify(inlineConfig) + ");"
+		});
+
+		loadScript({ src: 'assets/mathJaxInit.js' }, function() {
+			loadScript({ src: src }, function() {
+				Adapt.wait ? Adapt.wait.end() : Adapt.trigger("plugin:endWait");
+			});
+		});
 	}
 
 	function onProcessMath() {
@@ -27,11 +57,18 @@ define([ "core/js/adapt" ], function(Adapt) {
 	}
 
 	function onViewReady(view) {
-		var Hub = window.MathJax && window.MathJax.Hub;
-
 		$(".loading").show();
 
-		if (Hub) Hub.Queue([ "Typeset", Hub, view.el ]);
+		function checkForMathJax() {
+			if (!window.MathJax || !window.MathJax.Hub) {
+				window.setTimeout(checkForMathJax, 200);
+			} else {
+				var Hub = window.MathJax.Hub;
+				Hub.Queue([ "Typeset", Hub, view.el ]);
+			}
+		}
+
+		checkForMathJax();
 	}
 
 	function onPopupOpened($element) {
